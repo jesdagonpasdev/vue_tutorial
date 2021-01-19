4587803fc343d90304879b538b5241cc889d1006
Jesús David González Pascolo <jesdagonpas@gmail.com>
2021-01-19 10:05:20 +0000

    Data & Methods

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 new Vue({ 
     el:'#vue-app',          // Asocio la instancia al div que lo va a contener todo
     data: {                 // Declaracion de los datos que tendrá mi instancia y se bindearán al HTML.
-        name: 'Jesus'
+        name: 'Jesus',
+        job: 'Developer'
+    },
+    methods: {              // Declaración de los métodos de mi instancia
+        greet: function(time){
+            // Para insertar un valor de una variable de "data", valdría simplemente con poner this.name para extraerlo.
+            return 'Good ' + time + ' ' + this.name;
+        }
     }
 }); 
