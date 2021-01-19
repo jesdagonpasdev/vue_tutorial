90820ceada4c9b0a2b2001c122a55586f6553868
Jesús David González Pascolo <jesdagonpas@gmail.com>
2021-01-19 09:54:25 +0000

    Vue Instance

---
+++ app.js
@@ -0,0 +1,7 @@
+//Declaración de una instancia de Vue
+new Vue({ 
+    el:'#vue-app',          // Asocio la instancia al div que lo va a contener todo
+    data: {                 // Declaracion de los datos que tendrá mi instancia y se bindearán al HTML.
+        name: 'Jesus'
+    }
+}); 
