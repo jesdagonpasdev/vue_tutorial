bbf5d669c610a6267a2cfb3abc60e6444cb76a62
Jesús David González Pascolo <jesdagonpas@gmail.com>
2021-01-20 10:31:06 +0000

    Keyboard Events

--- app.js
+++ app.js
@@ -2,23 +2,14 @@
 new Vue({ 
     el:'#vue-app',          // Asocio la instancia al div que lo va a contener todo
     data: {                 // Declaracion de los datos que tendrá mi instancia y se bindearán al HTML.
-        age: 25,
-        x: 0,
-        y: 0
+
     },
     methods: {              // Declaración de los métodos de mi instancia
-        add: function(inc){
-            this.age += inc;
+        logName: function() {
+            console.log('you entered your name');
         },
-        substract: function(dec) {
-            this.age -= dec;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function(){
-            alert('You clicked me');
+        logAge: function() {
+            console.log('you entered your age');
         }
     }
 }); 
